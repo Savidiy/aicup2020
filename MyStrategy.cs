ebf54d68f1b2ce551fc6f46c237e4dd6acbc868f
Savidiy <savidiy@mail.ru>
2020-12-22 12:11:33 +0300

    фикс неправильный учет строящихся домов, всегда был 3

--- MyStrategy.cs
+++ MyStrategy.cs
@@ -2369,7 +2369,7 @@ namespace Aicup2020
                                 if ((populationMax <= 20 && count == 0)
                                     || (populationMax <= 40 && count <= 0)
                                     || (populationMax <= 60 && count <= 1)
-                                    || (count <= 2))
+                                    || (populationMax > 60 && count <= 2))
                                 {
                                     myResources -= newCost;
                                     plans.Add(PlanType.PlanCreateHouses);
